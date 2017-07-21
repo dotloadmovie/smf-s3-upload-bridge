a4e2c17ac73ce7c3d2c141e27f7ce857c675f135
Dave Tickle <dave@dotloadmovie.com>
2017-07-21 10:57:34 +0100

    test proxy data connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 class App extends Component {
+
+  componentDidMount() {
+
+    // sample AJAX request to test local dev proxy
+    // TODO: replace with Fetch polyfill
+    $.ajax({
+      url: 'service.php',
+      success: (response) => {
+        console.log(response);
+      }
+    });
+  }
+
+
   render() {
     return (
       <div className="app">
